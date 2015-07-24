71ae74ae7971a9087be7079217ca6e354e94ad69
sole <sole@mozilla.com>
2015-07-24 11:55:07 +0100

    shuffling some things around

---
+++ client/js/Threedes.js
@@ -0,0 +1,66 @@
+var THREE = require('n3d-threejs');
+
+function Threedees() {
+	var renderer;
+	var scene;
+	var camera;
+
+	this.init = function(htmlSlides) {
+
+		renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
+		renderer.shadowMapEnabled = true;
+		renderer.shadowMapSoft = true;
+
+		// Only expose the element, not the renderer
+		this.domElement = renderer.domElement;
+
+		scene = new THREE.Scene();
+		scene.add(new THREE.AmbientLight(0x444444));
+
+		camera = new THREE.PerspectiveCamera(60, 320 / 200, 1, 100000);
+		var cameraTarget = new THREE.Vector3( 0, 0, 0 );
+		camera.position.set(0, 0, 10);
+		camera.lookAt(cameraTarget);
+
+		var light = new THREE.DirectionalLight(0xdfebff, 1);
+		light.target.position.set(0, 0, 0);
+		light.position.set(0, 200, 0);
+		light.position.multiplyScalar(1.3);
+
+		light.castShadow = true;
+		light.shadowMapWidth = 1024;
+		light.shadowMapHeight = 1024;
+		var d = 400;
+		light.shadowCameraLeft = -d;
+		light.shadowCameraRight = d;
+		light.shadowCameraTop = d;
+		light.shadowCameraBottom = -d;
+
+		light.shadowCameraFar = 1000;
+		light.shadowDarkness = 0.5;
+		scene.add(light);
+
+		var n = 5;
+		var cubeGeometry = new THREE.BoxGeometry(n, n, n);
+		var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF });
+		cube = new THREE.Mesh(cubeGeometry, material);
+		cube.rotation.x = Math.PI / 4;
+		cube.castShadow = true;
+
+		scene.add(cube);
+
+	};
+
+	this.resize = function(w, h) {
+		renderer.setSize(w, h);
+		camera.aspect = w / h;
+		camera.updateProjectionMatrix();
+	};
+
+	this.render = function(time) {
+		renderer.render(scene, camera);
+	};
+
+}
+
+module.exports = Threedees;
--- client/js/main.js
+++ client/js/main.js
@@ -1,97 +1,27 @@
-var THREE = require('n3d-threejs');
-var rendererContainer;
-var renderer;
-var scene;
-var camera;
-var cube;
-var contentWidth, contentHeight;
+var Threedees = require('./Threedes');
+var slides;
 
-window.onload = function() {
-	init();
-};
+document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-	initGraphics();
+	console.log('ooh yea');
+
+	var htmlSlidesElement = document.querySelector('.slides');
+	slides = new Threedees();
+	slides.init(htmlSlidesElement);
+
+	var rendererContainer = document.getElementById('renderer');
+	rendererContainer.appendChild(slides.domElement);
 
 	window.addEventListener('resize', onWindowResized);
 
 	onWindowResized();
-
-	requestAnimationFrame(render);
-}
-
-function initGraphics() {
-
-	rendererContainer = document.getElementById('renderer');
-	renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
-	renderer.shadowMapEnabled = true;
-	renderer.shadowMapSoft = true;
-
-	rendererContainer.appendChild(renderer.domElement);
-
-	scene = new THREE.Scene();
-	scene.add(new THREE.AmbientLight(0x444444));
-
-
-	camera = new THREE.PerspectiveCamera(60, contentWidth / contentHeight, 1, 100000);
-	var cameraTarget = new THREE.Vector3( 0, 0, 0 );
-	camera.position.set(0, 0, 10);
-	camera.lookAt(cameraTarget);
-
-	
-	var light = new THREE.DirectionalLight(0xdfebff, 1);
-	//light.position.set(0, 100, 0);
-	light.target.position.set(0, 0, 0);
-	light.position.set(0, 200, 0);
-	light.position.multiplyScalar(1.3);
-
-	light.castShadow = true;
-	light.shadowMapWidth = 1024;
-	light.shadowMapHeight = 1024;
-	var d = 400;
-	light.shadowCameraLeft = -d;
-	light.shadowCameraRight = d;
-	light.shadowCameraTop = d;
-	light.shadowCameraBottom = -d;
-
-	light.shadowCameraFar = 1000;
-	light.shadowDarkness = 0.5;
-	scene.add(light);
-
-	var n = 5;
-	var cubeGeometry = new THREE.BoxGeometry(n, n, n);
-	var material = new THREE.MeshPhongMaterial({ color: 0xFFFFFF });
-	//var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-	cube = new THREE.Mesh(cubeGeometry, material);
-	//material.shading = THREE.FlatShading;
-	cube.rotation.x = Math.PI / 4;
-	cube.castShadow = true;
-	//cube.receiveShadow = true;
-
-	scene.add(cube);
-
-
-}
-
-function updateWindowDimensions() {
-	contentWidth = window.innerWidth;
-	contentHeight = window.innerHeight;
+	slides.render(0);
 }
 
 function onWindowResized() {
-	updateWindowDimensions();
-	renderer.setSize( contentWidth, contentHeight );
-	camera.aspect = contentWidth / contentHeight;
-	camera.updateProjectionMatrix();
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	slides.resize(w, h);
 }
 
-var lastTime = 0;
-
-function render(t) {
-	var delta = t - lastTime;
-	requestAnimationFrame(render);
-	cube.rotation.y += delta * 0.001;
-	cube.rotation.z -= delta * 0.001;
-	renderer.render(scene, camera);
-	lastTime = t;
-}
