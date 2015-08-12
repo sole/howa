bfa111a45ef3dae426455e2691bd1f56c93386ac
sole <sole@mozilla.com>
2015-08-12 19:28:44 +0100

    it works! (once)

--- client/js/scenes/web-audio-thx.js
+++ client/js/scenes/web-audio-thx.js
@@ -1,8 +1,15 @@
 module.exports = function(audioContext) {
 	var out = audioContext.createGain();
+	out.gain.value = 0.5;
+	
+	var oscillator = audioContext.createOscillator();
 
+	oscillator.connect(out);
+	
 	out.start = function() {
 		console.log('starting THX');
+		oscillator.start();
+		oscillator.stop(audioContext.currentTime + 0.5);
 	};
 
 	return out;
