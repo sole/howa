7216ad2181f13ac9ca693990413e3ec094688474
sole <sole@mozilla.com>
2015-08-31 21:32:36 +0200

    stereo panner to use colour palette + transition gain

--- client/js/scenes/stereo-panner.js
+++ client/js/scenes/stereo-panner.js
@@ -1,6 +1,8 @@
 module.exports = function(THREE, audioContext) {
 
 	var Renderable = require('../Renderable')(THREE);
+	var TransitionGain = require('./TransitionGain');
+	var colours = require('../colours');
 	
 	function StereoPanner() {
 		
@@ -8,21 +10,24 @@ module.exports = function(THREE, audioContext) {
 
 		var maxGain = 0.25;
 		var oscillator;
-		var gain = audioContext.createGain();
-		var stereoPanner = audioContext.createStereoPanner();
-		
-		gain.connect(stereoPanner);
+		var gain = TransitionGain(audioContext);
+		gain.connect(this.audioNode);
 
-		var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xFF0000 });
+		var stereoPanner = audioContext.createStereoPanner();
+		stereoPanner.connect(gain);
+
+
+		var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: colours.secondary1 });
 		var n = 5;
 		var geom = new THREE.SphereGeometry(n);
 		var mesh = new THREE.Mesh(geom, mat);
+		mesh.position.y -= n * 4;
 		this.add(mesh);
 
 		this.render = function(time) {
 			var now = audioContext.currentTime;
 			var pos = Math.sin(time * 0.001);
-			mesh.position.x = pos * 40;
+			mesh.position.x = pos * 80;
 			stereoPanner.pan.setValueAtTime(pos, now);
 		};
 	
@@ -30,23 +35,18 @@ module.exports = function(THREE, audioContext) {
 			var now = audioContext.currentTime;
 			oscillator = audioContext.createOscillator();
 			oscillator.frequency.setValueAtTime(330, now);
-			oscillator.connect(gain);
+			oscillator.connect(stereoPanner);
 			oscillator.start(now);
-			gain.gain.cancelScheduledValues(now);
-			gain.gain.setValueAtTime(0, now);
-			gain.gain.linearRampToValueAtTime(maxGain, now + 1);
-			stereoPanner.connect(this.audioNode);
+			gain.start();
 		};
 
 		this.deactivate = function() {
 			var now = audioContext.currentTime;
-			var t = 2;
-			gain.gain.cancelScheduledValues(now);
-			gain.gain.linearRampToValueAtTime(0, now + t);
-			setTimeout(function() {
+
+			gain.stop(function() {
+				oscillator.stop();
 				oscillator.disconnect();
-				stereoPanner.disconnect();
-			}, (t+0.5) * 1000);
+			});
 		};
 
 	}
