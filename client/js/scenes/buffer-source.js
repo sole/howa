1bd5e333b4914e83143bab03ee1594033aef287f
sole <sole@mozilla.com>
2015-08-31 20:32:27 +0200

    not using mesh in buffer source

--- client/js/scenes/buffer-source.js
+++ client/js/scenes/buffer-source.js
@@ -15,16 +15,8 @@ module.exports = function(THREE, audioContext) {
 		amenLoop.connect(gain);
 		amenLoop.sampler.loop = true;
 
-		var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xFF0000 });
-		var n = 5;
-		var geom = new THREE.SphereGeometry(n);
-		var mesh = new THREE.Mesh(geom, mat);
-		this.add(mesh);
-
 		this.render = function(time) {
 			var now = audioContext.currentTime;
-			var pos = Math.sin(time * 0.001);
-			mesh.position.x = pos * 40;
 		};
 	
 		this.activate = function() {
