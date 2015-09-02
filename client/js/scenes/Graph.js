4bee62aaa1a3d58b04e1199cee7488da0113de6e
sole <sole@mozilla.com>
2015-09-02 13:26:36 +0200

    graph but without text and without updating

---
+++ client/js/scenes/Graph.js
@@ -0,0 +1,82 @@
+module.exports = function(THREE, audioContext) {
+
+	var Renderable = require('../Renderable')(THREE);
+
+	function makeNode(text) {
+		var n = 5;
+		var mat = new THREE.MeshBasicMaterial({ wireframe: true, color: 0xFF00FF });
+		var geom = new THREE.BoxGeometry(n, n, n);
+		var obj = new THREE.Mesh(geom, mat);
+		return obj;
+	}
+
+	function makeEdge(node1, node2) {
+		var mat = new THREE.LineBasicMaterial({ color: 0x00FFFF });
+		var geom = new THREE.Geometry();
+		// will the line geometry update if the positions are updated? hoping so!
+		geom.vertices.push(node1.position);
+		geom.vertices.push(node2.position);
+		var line = new THREE.Line(geom, mat);
+		return line;
+	}
+
+	function Graph() {
+
+		Renderable.call(this, audioContext);
+
+	}
+
+	Graph.prototype = Object.create(Renderable.prototype);
+	Graph.prototype.constructor = Graph;
+
+
+	Graph.prototype.setData = function(nodes, edges) {
+		var self = this;
+		var nodes3D = [];
+		var edges3D = [];
+
+		nodes.forEach(function(text) {
+			var node = makeNode(text);
+			nodes3D.push(node);
+			self.add(node);
+			// TMP
+			var n = 30;
+			node.position.x = n * Math.random();
+			node.position.y = n * Math.random();
+
+		});
+
+		edges.forEach(function(edge) {
+			var node1 = nodes3D[edge[0]];
+			var node2 = nodes3D[edge[1]];
+			var edge3D = makeEdge(node1, node2);
+			self.add(edge3D);
+			edges3D.push(edge3D);
+		});
+
+		this.nodes3D = nodes3D;
+		this.edges3D = edges3D;
+		this.edges = edges;
+	};
+
+	Graph.prototype.render = function(time) {
+		// TODO
+	};
+
+	Graph.prototype.activate = function() {
+		// TODO
+		this.nodes3D.forEach(function(node) {
+			// tmp node.position.set(0, 0, 0);
+		});
+
+		// and distribute
+
+	};
+
+	Graph.prototype.deactivate = function() {
+		// TODO
+	};
+
+	
+	return Graph;
+};
--- client/js/scenes/audio-graph.js
+++ client/js/scenes/audio-graph.js
@@ -1,6 +1,45 @@
 module.exports = function(THREE, audioContext) {
 
 	var Renderable = require('../Renderable')(THREE);
+	var Graph = require('./Graph')(THREE, audioContext);
+
+	var nodes = [
+		'Oscillator',
+		'Destination'
+	];
+
+	var edges = [
+		[0, 1]
+	];
+
+	var nodes2 = [
+		'Oscillator',
+		'Oscillator',
+		'Oscillator',
+		'Destination'
+	];
+	var edges2 = [
+		[0, 3],
+		[1, 3],
+		[2, 3]
+	];
+
+	var nodes3 = [
+		'Oscillator', // 0
+		'Oscillator', // 1
+		'BiquadFilter', // 2
+		'WaveShaper', // 3
+		'Convolution', // 4
+		'Destination' // 5
+	];
+
+	var edges3 = [
+		[0, 2],
+		[1, 3],
+		[2, 4],
+		[3, 4],
+		[4, 5]
+	];
 
 	function SceneAudioGraph() {
 
@@ -11,6 +50,10 @@ module.exports = function(THREE, audioContext) {
 		var geom = new THREE.BoxGeometry(n, n, n);
 		var mesh = new THREE.Mesh(geom, mat);
 		this.add(mesh);
+
+		var graph = new Graph();
+		graph.setData(nodes3, edges3);
+		this.add(graph);
 
 		var lastTime = 0;
 		this.render = function(time) {
