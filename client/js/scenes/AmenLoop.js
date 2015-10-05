85c405f03879463be1a1fd8015b0cc155c5e43a7
sole <sole@mozilla.com>
2015-10-05 16:35:22 +0100

    taking assets out of the code for better compression (maybe)

--- client/js/scenes/AmenLoop.js
+++ client/js/scenes/AmenLoop.js
@@ -1,9 +1,34 @@
 var fs = require('fs');
 var Promise = require('es6-promise').Promise;
 var SamplePlayer = require('openmusic-sample-player');
-var amenSample = fs.readFileSync(__dirname + '/amen/amen.ogg');
 var decodedSample = null;
 var decodedPromise = null;
+
+var loadSample = function(context, samplePath) {
+	return new Promise(function(ok, fail) {
+		var req = new XMLHttpRequest();
+		req.open('get', samplePath, true);
+		req.responseType = 'arraybuffer';
+		req.addEventListener('error', function() {
+			fail(request.error);
+		});
+		req.addEventListener('load', function() {
+			var response = req.response;
+			if(response === null) {
+				fail('Empty response');
+			}
+
+			console.log('Attempting to decode');
+			context.decodeAudioData(response, function(buffer) {
+				console.log('decoded');
+				ok(buffer);
+			},  function(error) {
+				fail('Cannot decode sample at ' + samplePath + ' / ' + error);
+			});
+		});
+		req.send();
+	});
+};
 
 module.exports = function AmenLoop(audioContext) {
 
@@ -11,15 +36,7 @@ module.exports = function AmenLoop(audioContext) {
 	var sampler;
 
 	if(decodedPromise === null) {
-		decodedPromise = new Promise(function(ok, fail) {
-			audioContext.decodeAudioData(amenSample.toArrayBuffer(), function(buffer) {
-				console.log('amen promise');
-				ok(buffer);
-			}, function(error) {
-				console.error('cannot decode amen');
-				fail(error);
-			});
-		});
+		decodedPromise = loadSample(audioContext, 'data/amen/amen.ogg');
 	}
 
 	node = audioContext.createGain();
